b5cd6fafab80fd40cccfcb5a15315d03711d594a
Mario Garcia <mario@iessanvicente.com>
2021-04-19 19:35:00 +0200

    Version 1.1

--- Calculator.php
+++ Calculator.php
@@ -16,5 +16,12 @@ class Calculator
     {
         return $a / $b;
     }
- 
+
+    public function par($num): bool
+    {
+        if ($num%2==0)
+          return true;
+        else
+          return false;
+    } 
 }
